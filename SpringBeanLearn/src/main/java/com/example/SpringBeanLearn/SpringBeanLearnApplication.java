f1e1b555a2a154d85474b119e938a6a0ed20474b
CarlusInnocent <carlusinnocentainembabazi@gmail.com>
2025-10-27 21:00:34 +0300

    commit 21:00

---
+++ SpringBeanLearn/src/main/java/com/example/SpringBeanLearn/SpringBeanLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBeanLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeanLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeanLearnApplication.class, args);
+	}
+
+}
--- SpringBeanLearn/src/main/java/com/example/SpringBeanLearn/controllers/LoginController.java
+++ SpringBeanLearn/src/main/java/com/example/SpringBeanLearn/controllers/LoginController.java
@@ -1,4 +1,14 @@
 package com.example.SpringBeanLearn.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class LoginController {
+
+    @PostMapping("/login")
+    public String Login() {
+
+        return "login.html";
+    }
 }
